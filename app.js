7ca56e7702253d159407b1e8d0f431daccd776fb
antony <ant@energizedwork.com>
2015-09-11 12:20:28 +0100

    @antony #houskeeping Run project through xo to enforce good code style

--- app.js
+++ app.js
@@ -1,42 +1,36 @@
-var request = require('request'),
-    Hoek = require('hoek');
+var request = require('request');
+var Hoek = require('hoek');
 
-var apiKey = process.env.OPSGENIE_API_KEY,
-    apiUrl = process.env.OPSGENIE_API_URL || 'https://api.opsgenie.com',
-    policy = process.env.OPSGENIE_POLICY;
+var apiKey = process.env.OPSGENIE_API_KEY;
+var apiUrl = process.env.OPSGENIE_API_URL || 'https://api.opsgenie.com';
+var policy = process.env.OPSGENIE_POLICY;
 
-    Hoek.assert(apiKey, 'Opsgenie api key (env OPSGENIE_API_KEY) is required');
-    Hoek.assert(policy, 'Opsgenie policy name (env OPSGENIE_POLICY) is required');
+Hoek.assert(apiKey, 'Opsgenie api key (env OPSGENIE_API_KEY) is required');
+Hoek.assert(policy, 'Opsgenie policy name (env OPSGENIE_POLICY) is required');
 
 module.exports = {
-
-  trigger: function(mute) {
-
-    var operation = mute ? 'enable' : 'disable',
-        muted = mute ? 'muted': 'unmuted';
+  trigger: function (mute) {
+    var operation = mute ? 'enable' : 'disable';
+    var muted = mute ? 'muted' : 'unmuted';
 
     var req = {
-      method: "POST",
+      method: 'POST',
       url: apiUrl + '/v1/json/alert/policy/' + operation,
       body: {
         apiKey: apiKey,
         name: policy
       },
       json: true
-    }
+    };
 
-    request(req, function(error, response, resp) {
-
-      if (!error && response.statusCode == 200 && resp.status === "success") {
+    request(req, function (error, response, resp) {
+      if (!error && response.statusCode === 200 && resp.status === 'success') {
         console.log('Alerting', muted, 'successfully');
         process.exit(0);
       }
 
       console.error('Cannot', operation, 'alerting.', error || resp);
       process.exit(1);
-
     });
-
   }
-
 };
